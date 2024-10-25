dbf713b205fb4ab46bd7d4d1a57c7e365d2b3da8
Maurilio Nava Junior <mnavajunior@gmail.com>
2024-10-24 23:22:08 -0300

    Update DoublyLinkedList.py

--- python/DoublyLinkedList.py
+++ python/DoublyLinkedList.py
@@ -19,7 +19,40 @@ class DoublyLinkedList:
             aux_node = aux_node.next
 
     def append(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+        self.length += 1
+        return True
+
+    def pop(self, value):
         pass
+
+    def prepend(self, value):
+        pass
+
+    def pop_first(self, value):
+        pass
+
+    def get(self, value):
+        pass
+
+    def set(self, value):
+        pass
+
+    def insert(self, value):
+        pass
+
+    def remove(self, value):
+        pass
+
 
 doublyLinkedList = DoublyLinkedList(1)
 doublyLinkedList.print_list()
